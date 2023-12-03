95f70549da685035efaeb14d084a4fd2f3ff7554
felix-ha <hauerf98@gmail.com>
2023-12-03 23:57:26 +0100

    telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,9 +10,18 @@ return {
           vim.cmd([[colorscheme catppuccin]])
         end,
       },
+
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
-    }
+    },
+
+    {
+        'nvim-telescope/telescope.nvim',
+        lazy = true,
+        dependencies = {
+            {'nvim-lua/plenary.nvim'},
+        }
+    },
 
 }
