525801e2e56a5e2a3afad2bfc795145832c37554
felix-ha <hauerf98@gmail.com>
2023-12-03 23:09:00 +0100

    Install nvim-tree

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,39 @@
+return {
+
+    {
+        'akinsho/bufferline.nvim',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
+
+    {
+        'folke/tokyonight.nvim',
+    },
+
+    {
+        "phaazon/hop.nvim",
+        lazy = true,
+    },
+
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
+
+    {
+        'nvim-tree/nvim-tree.lua',
+        lazy = true,
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+        },
+    },
+
+    {
+        'folke/which-key.nvim',
+        lazy = true,
+    },
+
+}
