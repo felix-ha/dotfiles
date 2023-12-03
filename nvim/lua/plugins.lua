06ad1a9d61fd0ed05b73c1db6c26f42bdd912ef4
felix-ha <hauerf98@gmail.com>
2023-12-03 23:32:59 +0100

    Use other extenstion

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,5 +1,8 @@
 return {
 
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    }
 
 }
