583cd519f05604f685544de73b33863321e84679
felix-ha <hauerf98@gmail.com>
2023-12-03 23:13:29 +0100

    Just which key

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,39 +1,6 @@
 return {
 
-    {
-        'akinsho/bufferline.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons'
-        },
-    },
-
-    {
-        'folke/tokyonight.nvim',
-    },
-
-    {
-        "phaazon/hop.nvim",
-        lazy = true,
-    },
-
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons'
-        },
-    },
-
-    {
-        'nvim-tree/nvim-tree.lua',
-        lazy = true,
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-    },
-
-    {
-        'folke/which-key.nvim',
-        lazy = true,
-    },
+    "folke/which-key.nvim",
+    lazy = true,
 
 }
