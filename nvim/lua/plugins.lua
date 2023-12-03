2ed47ee89df75d6f2e6d1708716a950468bf674a
felix-ha <hauerf98@gmail.com>
2023-12-03 23:54:08 +0100

    catppuccin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,12 +1,13 @@
 return {
 
     {
-        "folke/tokyonight.nvim",
+        "catppuccin/nvim",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        priority = 1000,
+        flavour = "mocha" -- make sure to load this before all the other start plugins
         config = function()
           -- load the colorscheme here
-          vim.cmd([[colorscheme tokyonight]])
+          vim.cmd([[colorscheme catppuccin]])
         end,
       },
     {
