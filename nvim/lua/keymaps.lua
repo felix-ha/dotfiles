24db43e59947a66a771c3ae9c7ef679049e39322
felix-ha <hauerf98@gmail.com>
2023-12-06 22:47:51 +0100

    Fix

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -29,7 +29,7 @@ keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
 
