19637dd12b27dadfd5dee27951b747694f29d342
felix-ha <hauerf98@gmail.com>
2023-12-09 21:59:27 +0100

    Next try

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,5 +2,6 @@ require "user.keymaps"
 require "user.options"
 require "user.lazy-config"
 require 'user.lualine'
-require 'user.nvim-tree' 
 require 'user.telescope'
+
+require("nvim-tree").setup() -- nvim tree setup here because netrw showed up
