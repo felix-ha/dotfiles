6b22dc6c58eb4f66d13670ebbe92d956ffb8f65f
felix-ha <hauerf98@gmail.com>
2023-12-09 21:45:20 +0100

    Use LunarVim structur

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,6 @@
 require "user.keymaps"
 require "user.options"
 require "user.lazy-config"
-
-require('user.lualine').setup()
-require('user.nvim-tree').setup()
-require('user.telescope').setup()
-require('user.nvim-web-devicons').setup()
+require 'user.lualine'
+require 'user.nvim-tree' 
+require 'user.telescope'
