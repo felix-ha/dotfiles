925c7a25371941f31b7582b05eef4555ef321260
felix-ha <hauerf98@gmail.com>
2023-12-09 21:55:39 +0100

    Disable netrw

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -11,6 +11,8 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  disable_netrw = true,
+  hijack_netrw = true,
   update_focused_file = {
     enable = true,
     update_cwd = true,
