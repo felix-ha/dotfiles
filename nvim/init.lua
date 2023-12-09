33c6034b8a488d68e62b4536af0564f56af77e4b
felix-ha <hauerf98@gmail.com>
2023-12-09 22:00:55 +0100

    Use cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@ require "user.options"
 require "user.lazy-config"
 require 'user.lualine'
 require 'user.telescope'
+require 'user.cmp'
 
 require("nvim-tree").setup() -- nvim tree setup here because netrw showed up
