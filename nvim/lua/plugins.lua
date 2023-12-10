0821271d22ec9adb7579d1763b8c419e80dfcbdc
felix-ha <hauerf98@gmail.com>
2023-12-10 21:32:44 +0100

    Typo

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -56,7 +56,7 @@ return {
 
     {
         -- LSP
-        "neovim/nvim-lspconfig"k -- enable LSP
+        "neovim/nvim-lspconfig", -- enable LSP
         "williamboman/mason.nvim", -- simple to use language server installer
         "williamboman/mason-lspconfig.nvim", -- simple to use language server installer
         'jose-elias-alvarez/null-ls.nvim', -- LSP diagnostics and code actions
