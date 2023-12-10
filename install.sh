c66d5c132066301eab3b36b022843e30cdad3379
felix-ha <hauerf98@gmail.com>
2023-12-10 22:21:55 +0100

    Do not install zsh

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@ git clone https://github.com/felix-ha/dotfiles.git ~/.dotfiles
 
 apt update
 
-# zsh
-apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-ln -s -f ~/.dotfiles/.zshrc ~/.zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 # neovim
 # apt install neovim -y
 apt install libfuse2 ripgrep -y
