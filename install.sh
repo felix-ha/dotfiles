39a8b35b01c294c759ad23feec8fae383fb1bcd5
Felix Jobson <hauerf98@gmail.com>
2024-01-09 21:56:15 +0100

    Check if directory exists

--- install.sh
+++ install.sh
@@ -10,5 +10,12 @@ chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 mv squashfs-root /
 ln -s /squashfs-root/AppRun /usr/bin/nvim
-mkdir ~/.config
+
+if [[ -d "~/.config" ]]; then
+    echo "~/.config exists"
+else
+    echo "creating dir ~/.config"
+    mkdir ~/.config
+fi
+
 ln -s ~/.dotfiles/nvim ~/.config/nvim
