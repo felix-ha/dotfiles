4d488c3a24140cdab1813eb86ebddb42b2fef123
Felix Jobson <hauerf98@gmail.com>
2024-01-29 08:09:51 +0100

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,3 +14,13 @@ local keymap = vim.api.nvim_set_keymap
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
+
+-- ToggleTerm --
+keymap("n", "<leader>tt", "<cmd>ToggleTerm<cr>", opts)
+keymap("n", "<leader>ttf", "<cmd>ToggleTerm direction=float<cr>", opts)
+keymap("n", "<C-_>", "<cmd>ToggleTerm direction=float<cr>", opts)
+keymap("t", "jk", [[<C-\><C-n>]], opts)
+
+-- Telescope --
+keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
