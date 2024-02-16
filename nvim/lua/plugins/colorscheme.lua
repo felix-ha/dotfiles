a0fae41bead25b17642812c5ad0445cd02a87c3d
Felix Jobson <hauerf98@gmail.com>
2024-02-16 21:56:41 +0100

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
 
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "Mofiqul/vscode.nvim", priority = 1000 },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "vscode",
     },
   }
 }
