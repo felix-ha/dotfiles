7fb77d256d49a39e2b09ad8ed609965031739b77
Felix Jobson <hauerf98@gmail.com>
2024-02-19 22:48:29 +0100

    Need npm for lsp

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt update
 
 # neovim
 # apt install neovim -y
-apt install libfuse2 ripgrep fd-find g++ -y
+apt install libfuse2 ripgrep fd-find g++ npm -y
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
