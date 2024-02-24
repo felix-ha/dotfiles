9d0a8dfa166e823504f97f289a3970eaed76f9bb
Felix Jobson <hauerf98@gmail.com>
2024-02-24 21:32:32 +0100

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,11 +15,20 @@ local keymap = vim.api.nvim_set_keymap
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
 
+-- ZenMode --
+keymap("n", "<leader>z", "<cmd>ZenMode<cr>", opts)
+
 -- ToggleTerm --
-keymap("n", "<leader>tt", "<cmd>ToggleTerm<cr>", opts)
-keymap("n", "<leader>ttf", "<cmd>ToggleTerm direction=float<cr>", opts)
-keymap("n", "<C-_>", "<cmd>ToggleTerm direction=float<cr>", opts)
+keymap("n", "<leader>ttf", "<cmd>ToggleTerm direction=float dir=git_dir<cr>", opts)
+keymap("n", "<leader>tth", "<cmd>ToggleTerm direction=horizontal dir=git_dir<cr>", opts)
+keymap("n", "<leader>ttv", "<cmd>ToggleTerm direction=vertical dir=git_dir<cr>", opts)
+keymap("n", "<C-_>", "<cmd>ToggleTerm direction=float dir=git_dir<cr>", opts)
 keymap("t", "jk", [[<C-\><C-n>]], opts)
+keymap("n", "<leader>ttl", "<cmd>ToggleTermSendCurrentLine<cr>", opts)
+keymap("v", "<leader>ttv", "<cmd>ToggleTermSendVisualLines<cr>", opts)
+keymap("v", "<leader>ttV", "<cmd>ToggleTermSendVisualSelection<cr>", opts)
+terminal_cmd = os.getenv("TERMINAL_CMD") or "ls -lisah"
+keymap("n", "<F5>", string.format("<cmd>TermExec cmd='%s' direction=float<cr>", terminal_cmd), opts)
 
 -- Telescope --
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
