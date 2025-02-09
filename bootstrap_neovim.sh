c3bcb618d63dba2c0fa367cfcf0d8ce5bef3833f
Felix Jobson <hauerf98@gmail.com>
2025-02-09 20:39:05 +0100

    Update bootstrap_neovim.sh

--- bootstrap_neovim.sh
+++ bootstrap_neovim.sh
@@ -2,13 +2,12 @@ NVIM_VERSION="v0.10.4"
 
 set -e 
 apt update
-apt install git curl -y
+apt install git curl fuse libfuse2 -y
 git clone https://github.com/felix-ha/dotfiles.git ~/.dotfiles
 
-curl -LO https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux-x86_64.tar.gz
-tar xzvf nvim-linux-x86_64.tar.gz
-mv nvim-linux-x86_64 /opt/nvim
-ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
 
 apt install git make g++ ripgrep -y
 [[ -d ~/.config ]] || mkdir ~/.config
