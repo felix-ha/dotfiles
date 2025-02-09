e8b5549c446ced5fb0f91b0f83b90776c2091abc
Felix Jobson <hauerf98@gmail.com>
2025-02-09 20:29:33 +0100

    Update bootstrap_neovim.sh

--- bootstrap_neovim.sh
+++ bootstrap_neovim.sh
@@ -10,17 +10,16 @@ git clone https://github.com/felix-ha/dotfiles.git ~/.dotfiles
 # apt installs neovim version 0.9.5 at the time of writing this
 # apt install neovim -y 
 
-# is broken
-#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-#chmod u+x nvim.appimage
-#./nvim.appimage --appimage-extract
-#mv squashfs-root /
-#ln -s /squashfs-root/AppRun /usr/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+mv squashfs-root /
+ln -s /squashfs-root/AppRun /usr/bin/nvim
 
-apt install software-properties-common -y
-add-apt-repository ppa:neovim-ppa/unstable -y
-apt update
-apt install neovim -y
+#apt install software-properties-common -y
+#add-apt-repository ppa:neovim-ppa/unstable -y
+#apt update
+#apt install neovim -y
 
 apt install git make g++ ripgrep -y
 
