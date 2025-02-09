c8d281277b20b55a4374a58300460ea6e71f54b5
Felix Jobson <hauerf98@gmail.com>
2025-02-09 20:17:52 +0100

    Update and rename install.sh to bootstrap_neovim.sh

--- bootstrap_neovim.sh
+++ bootstrap_neovim.sh
@@ -1,18 +1,28 @@
-git clone https://github.com/felix-ha/dotfiles.git ~/.dotfiles
+set -e 
 
 apt update
 
+apt install git -y
 
-#apt install libfuse2 ripgrep fd-find g++ npm -y
+git clone https://github.com/felix-ha/dotfiles.git ~/.dotfiles
 
 # neovim
-apt install neovim -y
+# apt installs neovim version 0.9.5 at the time of writing this
+# apt install neovim -y 
 
+# is broken
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 #chmod u+x nvim.appimage
 #./nvim.appimage --appimage-extract
 #mv squashfs-root /
 #ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+apt install software-properties-common -y
+add-apt-repository ppa:neovim-ppa/unstable -y
+apt update
+apt install neovim -y
+
+apt install git make g++ ripgrep -y
 
 if [[ -d ~/.config ]]; then
     echo "~/.config exists"
