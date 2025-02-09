75bff367f94797cc15020c064dce446a5f1a0610
Felix Jobson <hauerf98@gmail.com>
2025-02-09 21:19:01 +0100

    Update bootstrap_neovim.sh

--- bootstrap_neovim.sh
+++ bootstrap_neovim.sh
@@ -1,17 +1,19 @@
 NVIM_VERSION="v0.10.4"
 
 set -e 
-apt update
-apt install git curl fuse libfuse2 -y
+
+apt update && apt install -y git cmake g++ pkg-config libtool-bin gettext
+git clone https://github.com/neovim/neovim.git
+cd neovim && git checkout $NVIM_VERSION
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+make install
+cd ..
+rm -rf neovim
+
 git clone https://github.com/felix-ha/dotfiles.git ~/.dotfiles
 
-curl -LO https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux-x86_64.appimage
-chmod u+x nvim-linux-x86_64.appimage
-./nvim-linux-x86_64.appimage --appimage-extract
-mv squashfs-root /opt/nvim
-ln -s /opt/nvim/usr/bin/nvim /usr/local/bin/nvim
+apt install g++ ripgrep -y
 
-apt install git make g++ ripgrep -y
 [[ -d ~/.config ]] || mkdir ~/.config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 echo 'alias v="nvim"' >> ~/.bashrc
