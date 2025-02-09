ae1edd4379ecae1a6c03009bb2b15e9814e1038e
Felix Jobson <hauerf98@gmail.com>
2025-02-09 20:33:56 +0100

    Update bootstrap_neovim.sh

--- bootstrap_neovim.sh
+++ bootstrap_neovim.sh
@@ -1,35 +1,16 @@
+NVIM_VERSION="v0.10.4"
+
 set -e 
-
 apt update
-
-apt install git -y
-
+apt install git curl -y
 git clone https://github.com/felix-ha/dotfiles.git ~/.dotfiles
 
-# neovim
-# apt installs neovim version 0.9.5 at the time of writing this
-# apt install neovim -y 
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-mv squashfs-root /
-ln -s /squashfs-root/AppRun /usr/bin/nvim
-
-#apt install software-properties-common -y
-#add-apt-repository ppa:neovim-ppa/unstable -y
-#apt update
-#apt install neovim -y
+curl -LO https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux-x86_64.tar.gz
+tar xzvf nvim-linux-x86_64.tar.gz
+mv nvim-linux-x86_64 /opt/nvim
+ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
 
 apt install git make g++ ripgrep -y
-
-if [[ -d ~/.config ]]; then
-    echo "~/.config exists"
-else
-    echo "creating dir ~/.config"
-    mkdir ~/.config
-fi
-
+[[ -d ~/.config ]] || mkdir ~/.config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
-
 echo 'alias v="nvim"' >> ~/.bashrc
